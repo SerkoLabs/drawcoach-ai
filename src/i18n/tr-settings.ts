67b81f41d8624d190b667a4c50fed9c46f6947b9
SerkanDinc <serkan@serkandinc.com>
2026-09-07 20:04:20 +0300

    feat: add settings translations

---
+++ src/i18n/tr-settings.ts
@@ -0,0 +1,5 @@
+export const trSettings = {
+  'settings.signOut': 'Çıkış Yap',
+  'settings.signingOut': 'Çıkış yapılıyor…',
+  'settings.signOutError': 'Çıkış yapılamadı. Tekrar dene.',
+} as const;
